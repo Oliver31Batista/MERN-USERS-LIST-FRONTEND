a9755909d122d94b3413e849858cba53f9e4a159
Oliver31Batista <ohenriquez@intellisys.com.do>
2022-08-09 11:44:24 -0400

    create users working

--- src/components/CrearUsuario.jsx
+++ src/components/CrearUsuario.jsx
@@ -1,10 +1,46 @@
 import React from "react";
+import { useState } from "react";
+import axios from 'axios'
 
 const CrearUsuario = () => {
+
+  const valorInicial = {
+    nombre: '',
+    apellido: '',
+    edad: 18,
+    telefono: 0,
+    correo: ''
+  }
+
+  const [usuario, setUsuario] = useState(valorInicial)
+
+  const capturarDatos = (e) => {
+    const {name, value} = e.target
+    setUsuario({...usuario, [name]: value})//...usuario, trae una copia de todo lo que hay en la variable de estado
+  }
+
+  const guardarDatos = async (e) => {
+    e.preventDefault();
+    
+    //logica para la peticion post
+
+    const nuevoUsuario = {
+      nombre: usuario.nombre,
+      apellido: usuario.apellido,
+      edad: usuario.edad,
+      telefono: usuario.telefono,
+      correo: usuario.correo
+    }
+
+    await axios.post('http://localhost:4000/api/usuarios', nuevoUsuario)
+
+    setUsuario({...valorInicial})
+  }
+
   return (
     <div className="col-md-6 offset-md-3">
       <div className="card card-body">
-        <form>
+        <form onSubmit={guardarDatos}>
           <h1 className="text-center mb-3">Crear Usuario</h1>
           <div className="mb-3">
             <label>Nombre:</label>
@@ -14,6 +50,9 @@ const CrearUsuario = () => {
               className="form-control"
               placeholder="Ingrese el nombre del usuario"
               required
+              name='nombre'
+              value = {usuario.nombre}
+              onChange={capturarDatos}
             />
           </div>
 
@@ -25,6 +64,9 @@ const CrearUsuario = () => {
               className="form-control"
               placeholder="Ingrese el apellido del usuario"
               required
+              name='apellido'
+              value = {usuario.apellido}
+              onChange={capturarDatos}
             />
           </div>
 
@@ -36,6 +78,9 @@ const CrearUsuario = () => {
               className="form-control"
               placeholder="Ingrese la edad del usuario"
               required
+              name='edad'
+              value = {usuario.edad}
+              onChange={capturarDatos}
             />
           </div>
 
@@ -47,6 +92,9 @@ const CrearUsuario = () => {
               className="form-control"
               placeholder="Ingrese el teléfono del usuario"
               required
+              name='telefono'
+              value = {usuario.telefono}
+              onChange={capturarDatos}
             />
           </div>
 
@@ -58,6 +106,9 @@ const CrearUsuario = () => {
               className="form-control"
               placeholder="Ingrese el correo del usuario"
               required
+              name='correo'
+              value = {usuario.correo}
+              onChange={capturarDatos}
             />
           </div>
 

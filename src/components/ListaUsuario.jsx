1982019c15477fa28338b5595c90206ffe4b935d
Oliver31Batista <ohenriquez@intellisys.com.do>
2022-08-09 12:09:01 -0400

    users list working

--- src/components/ListaUsuario.jsx
+++ src/components/ListaUsuario.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const ListaUsuario = () => {
-  return (
-    <div>ListaUsuario</div>
-  )
-}
+  const [lista, setLista] = useState([]);
 
-export default ListaUsuario
+  useEffect(() => {
+    const getUsuarios = async () => {
+      const res = await axios.get("http://localhost:4000/api/usuarios");
+      setLista(res.data);
+    };
+    getUsuarios();
+  }, [lista]);
+
+  return (
+    <div className="row">
+      {lista.map(listItem => (
+        <div className="col-md-4 p-2" key={listItem._id}>
+          <div className="card">
+            <div className="card-header">
+              <h5>Nombre: {listItem.nombre}</h5>
+            </div>
+            <div className="card-body">
+              <p>Apellido: {listItem.apellido}</p>
+              <p>Edad: {listItem.edad}</p>
+              <p>Telefono: {listItem.telefono}</p>
+              <p>Correo: {listItem.correo}</p>
+            </div>
+            <div className="card-footer">
+              <button className="btn btn-danger">
+                Eliminar
+              </button>
+            </div>
+          </div>
+        </div>
+))}
+    </div>
+  );
+};
+
+export default ListaUsuario;
